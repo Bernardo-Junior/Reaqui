3f5654e39689188702077fe31eaaacbcd4d66b9f
Bernardo <bernardo@iMac-Pro-de-Bernardo.local>
2021-06-05 15:49:41 -0400

    chore: importando imagens e icones

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,12 +4,16 @@ import 'react-native-gesture-handler';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 
 import Routes from '../infra/services/routes'
+import { StatusBar } from 'react-native';
 
 const App: React.FC = () => {
   return (
+    <>
+    <StatusBar backgroundColor="#C8D7DB" barStyle="dark-content" animated={true} />
     <NavigationContainer>
       <Routes />
     </NavigationContainer>
+    </>
   )
 }
 
