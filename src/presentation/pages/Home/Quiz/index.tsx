f2281d3fced07306a0a59d38ace614ccc696cb4e
Bernardo <bernardo@iMac-Pro-de-Bernardo.local>
2021-06-14 20:07:21 -0400

    fix: adicionando perguntas e fazendo tela de placar

--- src/presentation/pages/Home/Quiz/index.tsx
+++ src/presentation/pages/Home/Quiz/index.tsx
@@ -7,6 +7,8 @@ import iconLivro from '../../../../assets/icons/livro.png';
 
 import { Data } from '../../../../utils/perguntasArray';
 
+import resp from '../../../../utils/responsivity';
+
 import {
   Header_Container,
   Header_Text,
@@ -38,6 +40,7 @@ const Quiz: React.FC = () => {
   const { goBack, } = useNavigation();
   const [control, setControl] = useState<number>(0);
   const [pontuacao, setPontuacao] = useState<number>(0);
+  const [questoesCorretas, setQuestoesCorretas] = useState<number>(0);
 
   // useEffect(() => {
   //   const gestureEndListener = () => {
@@ -50,7 +53,7 @@ const Quiz: React.FC = () => {
   //   return () => {
   //     navigation.removeListener('gestureEnd', gestureEndListener);
   //   };
-  // }, []);
+  // }, [])
 
   useEffect(() => {
     const backAction = () => {
@@ -83,9 +86,27 @@ const Quiz: React.FC = () => {
           
         }
       })
+      setPontuacao(0);
+      setQuestoesCorretas(0);
+      setQuantidadeQuestoes(0);
     }
     setControl(control+1);
     goBack();
+  }
+
+  const reset = () => {
+    console.log(quantidadeQuestoes)
+    for(var i = 0; i <= quantidadeQuestoes; i++) {
+      Data[i].opcoes.forEach(op => {
+        if(op.isSelecionado === true) {
+          op.isSelecionado = false;
+          
+        }
+      })
+      setPontuacao(0);
+      setQuestoesCorretas(0);
+      setQuantidadeQuestoes(0);
+    }
   }
   const pintarRadio = (id: number, index: number) => {
     const buscarIndex = Data[quantidadeQuestoes].opcoes.findIndex(op => op.id === id);
@@ -102,23 +123,30 @@ const Quiz: React.FC = () => {
     if(indexSelecionado >= 0) {
       console.log(indexSelecionado, Data[quantidadeQuestoes].respota)
       if(indexSelecionado === Data[quantidadeQuestoes].respota) {
-        setPontuacao(pontuacao+1);
-        setQuantidadeQuestoes(quantidadeQuestoes+1);
+        if(quantidadeQuestoes <= 9) {
+          setPontuacao(pontuacao+10);
+          setQuestoesCorretas(questoesCorretas+1);
+          setQuantidadeQuestoes(quantidadeQuestoes+1);
+        } else {
+          setPontuacao(pontuacao+1);
+          setQuestoesCorretas(questoesCorretas+1);
+        }
+      } else {
+        if(quantidadeQuestoes <= 9) {
+          setQuantidadeQuestoes(quantidadeQuestoes+1);
+        } 
       }
     } else {
+      
       console.log("CAMPO VAZIO")
     }
   }
   useEffect(() => {
    
   }, [control])
-  return (
-    <>
-      {
-        Platform.OS === "ios" && (
-          <StatusBar />
-        )
-      }
+
+  const render_questoes = () => {
+    return (
       <Global_Scroll style={{ backgroundColor: '#F7F7F7' }}>
         <Global_Container >
           <Header_Container>
@@ -202,6 +230,86 @@ const Quiz: React.FC = () => {
           
         </Global_Container>
       </Global_Scroll>
+    )
+  }
+
+  const render_placar = () => {
+    return (
+      <Global_Scroll style={{ backgroundColor: '#F7F7F7' }}>
+      <Global_Container >
+        <Header_Container>
+          <Header_Icon
+            source={iconLivro}
+            resizeMode="stretch"
+          />
+          <Header_Text>
+            Quiz Reaqui
+          </Header_Text>
+        </Header_Container>
+
+        <Questao_Container style={[Global_styles.shadow, { elevation: 2, backgroundColor: '#1DD1A1'}]}>
+          <Questao_Header style={{ backgroundColor: '#1DD1A1'}}>
+            <Questao_Header_Container>
+              <Titulo_Questao style={{color: "#FFFFFF"}}>
+                Parabéns!
+              </Titulo_Questao>
+              <Pergunta_Texto style={{color: "#FFFFFF", lineHeight: resp(25)}}>
+               Você acertou um total de {questoesCorretas} questões, em um total de 10 questões.
+               {`\n`}Continue praticando e fazendo o seu melhor.
+              </Pergunta_Texto>
+            </Questao_Header_Container>
+          </Questao_Header>
+          
+        </Questao_Container>
+        <Footer_Container>
+          <Footer_Container_Opcoes_Top>
+            <Footer_Container_Opcoes_Pontuacao_Text>
+              Pontuação: <Text style={{fontWeight: 'bold', color: "#000000"}}>{pontuacao}</Text>
+            </Footer_Container_Opcoes_Pontuacao_Text>
+            <Footer_Container_Opcoes_Button_Next  
+              style={[Global_styles.shadow, { elevation: 2, width: resp(130), }]}
+              onPress={() => { reset() }}
+            >
+              <Footer_Container_Opcoes_Button_Label>
+                Jogar de novo
+              </Footer_Container_Opcoes_Button_Label>
+            </Footer_Container_Opcoes_Button_Next>
+          </Footer_Container_Opcoes_Top>
+          <Button_Sair 
+            style={[Global_styles.shadow, { elevation: 2}]}
+            onPress={() => { 
+              Alert.alert("Sair!", "Você perderá todo o seu progresso se sair, deseja mesmo sair", [
+                {
+                  text: "Não",
+                  onPress: () => null,
+                  style: "cancel"
+                },
+                { text: "Sim", onPress: () => { limparTudo()} }
+              ]);
+            }}
+          >
+            <Footer_Container_Opcoes_Button_Label>
+              Sair
+            </Footer_Container_Opcoes_Button_Label>
+          </Button_Sair>
+        </Footer_Container>
+        
+      </Global_Container>
+    </Global_Scroll>
+    )
+  }
+  return (
+    <>
+      {
+        Platform.OS === "ios" && (
+          <StatusBar />
+        )
+      }
+      {
+        quantidadeQuestoes <= 9
+        ? ( render_questoes() )
+        : ( render_placar() )
+      }
     </>
   )
 }
--- src/utils/perguntasArray.tsx
+++ src/utils/perguntasArray.tsx
@@ -2,28 +2,28 @@ var Data = [
   {
     id: 1,
     titulo: "Questão 1",
-    pergunta: "Qual o resultado da reação A + B ?",
-    respota: 0,
+    pergunta: "Ao realizar a reação de adição do composto pent-2-eno com o ácido clorídrico (HCI) qual será o composto formado?",
+    respota: 2,
     opcoes: [
+      {
+        id: 0,
+        isSelecionado: false,
+        texto: "2-cloropentano"
+      }, 
       {
         id: 1,
         isSelecionado: false,
-        texto: "B + C"
-      }, 
+        texto: "1-cloropentano"
+      },
       {
         id: 2,
         isSelecionado: false,
-        texto: "Nada acontece"
+        texto: "3-cloropentano"
       },
       {
         id: 3,
         isSelecionado: false,
-        texto: "Um ácido é gerado"
-      },
-      {
-        id: 4,
-        isSelecionado: false,
-        texto: "Nenhuma das alternativas"
+        texto: "4-cloropentano"
       },
       
     ]
@@ -31,32 +31,270 @@ var Data = [
   {
     id: 2,
     titulo: "Questão 2",
-    pergunta: "Qual o resultado da reação A + B ?",
+    pergunta: "A hidratação do propeno dará origem a qual dos compostos abaixo?",
     respota: 0,
     opcoes: [
       {
+        id: 0,
+        isSelecionado: false,
+        texto: "Propan-2-ol"
+      }, 
+      {
         id: 1,
         isSelecionado: false,
-        texto: "B + C"
-      }, 
+        texto: "Propan-1-ol"
+      },
       {
         id: 2,
         isSelecionado: false,
-        texto: "Nada acontece"
+        texto: "Ácido propanoico"
       },
       {
         id: 3,
         isSelecionado: false,
-        texto: "Um ácido é gerado"
-      },
-      {
-        id: 4,
-        isSelecionado: false,
-        texto: "Nenhuma das alternativas"
+        texto: "Propanona"
       },
       
     ]
-  }
+  },
+  {
+    id: 3,
+    titulo: "Questão 3",
+    pergunta: "A hidrogenação catalítica de uma ligação dupla é caracterizada como uma reação de:",
+    respota: 3,
+    opcoes: [
+      {
+        id: 0,
+        isSelecionado: false,
+        texto: "Substituição"
+      }, 
+      {
+        id: 1,
+        isSelecionado: false,
+        texto: "Eliminação"
+      },
+      {
+        id: 2,
+        isSelecionado: false,
+        texto: "Transesteriicação"
+      },
+      {
+        id: 3,
+        isSelecionado: false,
+        texto: "Adição"
+      },
+      
+    ]
+  },
+  {
+    id: 4,
+    titulo: "Questão 4",
+    pergunta: "Assinale a única alternativa que indica uma reação de decomposição:",
+    respota: 0,
+    opcoes: [
+      {
+        id: 0,
+        isSelecionado: false,
+        texto: "2 HgO→2 Hg+O2"
+      }, 
+      {
+        id: 1,
+        isSelecionado: false,
+        texto: "HBr+KOH→KBr+H2O"
+      },
+      {
+        id: 2,
+        isSelecionado: false,
+        texto: "CaO+H2O→Ca(OH)2"
+      },
+      {
+        id: 3,
+        isSelecionado: false,
+        texto: "2 Na+ZnCl2→NaCl+Zn"
+      },
+      
+    ]
+  },
+  {
+    id: 5,
+    titulo: "Questão 5",
+    pergunta: "Das reações químicas que ocorrem:     \n\nI. nos flashes fotográficos descartáveis; \nII. com o fermento químico para fazer bolos; \nIII. no ataque de ácido clorídrico ao ferro; \nIV. na formação de hidróxido de alimínio usado no tratamento da água; \nV. na câmara de gás; \nRepresentadas, respectivamente, pelas equações: \n\nI. 2 Mg+O2→2 MgO \nII. NH4HCO3→CO2+NH3+H2O \nIII. Fe+2 HCl→FeCl2+H2 \nIV. Al2(SO4)3+6 NaOH→2 Al(OH)3+3 Na2SO4 \nV. H2SO4+2 KCN→K2SO4+2 HCN \n\nAssinale a alternativa que corresponde a reações de decomposição:",
+
+    respota: 1,
+    opcoes: [
+      {
+        id: 0,
+        isSelecionado: false,
+        texto: "a) apenas I e III"
+      }, 
+      {
+        id: 1,
+        isSelecionado: false,
+        texto: "b) apenas II"
+      },
+      {
+        id: 2,
+        isSelecionado: false,
+        texto: "c) apenas I e II"
+      },
+      {
+        id: 3,
+        isSelecionado: false,
+        texto: "d) apenas III, IV e V"
+      },
+      
+    ]
+  },
+  {
+    id: 6,
+    titulo: "Questão 6",
+    pergunta: "O consumo de ácido sulfúrico pode ser utilizado como um indicador do desenvolvimento de um país. Industrialmente, esse ácido pode ser obtido a partir da pirita de ferro, que consiste basicamente em sulfeto ferroso (FeS). Classifique as equações de obtenção industrial do ácido sulfúrico mostradas a seguir: \n\nI. FeS+O2→Fe+SO2 \nII. 2 SO2+2 O2→2 SO3 \nIII. SO3+H2O→H2SO4",
+
+    respota: 2,
+    opcoes: [
+      {
+        id: 0,
+        isSelecionado: false,
+        texto: "a) dupla troca, análise, análise"
+      }, 
+      {
+        id: 1,
+        isSelecionado: false,
+        texto: "b) síntese, simples troca, dupla troca"
+      },
+      {
+        id: 2,
+        isSelecionado: false,
+        texto: "c) simples troca, síntese, síntese"
+      },
+      {
+        id: 3,
+        isSelecionado: false,
+        texto: "d) simples troca, análise, análise"
+      },
+      
+    ]
+  },
+  {
+    id: 7,
+    titulo: "Questão 7",
+    pergunta: "Fazendo-se a classificação das reações abaixo: \n\nI. CuSO4+2 NaOH→Cu(OH)2+Na2SO4 \nII. Cu(OH)2→CuO+H2O \nIII. Zn+2 AgNBaCO3→2 Ag+Zn(NBaCO3)2 \nIV. NH3+HCl→NH4Cl",
+
+    respota: 0,
+    opcoes: [
+      {
+        id: 0,
+        isSelecionado: false,
+        texto: "a) dupla troca, análise, deslocamento, síntese"
+      }, 
+      {
+        id: 1,
+        isSelecionado: false,
+        texto: "b) deslocamento, análise, dupla troca, adição"
+      },
+      {
+        id: 2,
+        isSelecionado: false,
+        texto: "c) dupla troca, decomposição, síntese, simples troca"
+      },
+      {
+        id: 3,
+        isSelecionado: false,
+        texto: "d) decomposição, simples troca, dupla troca, adição"
+      },
+      
+    ]
+  },
+  {
+    id: 8,
+    titulo: "Questão 8",
+    pergunta: "Reações de deslocamento ou simples troca são aquelas em que uma sustância simples ou de um elemento mais reativo desloca outra de uma substância composta. Um exemplo de reação de deslocamento, em que o cálcio desloca o hidrogênio, é apresentado a seguir: \n\nCa(s)+2 HNO3(aq)→Ca(NO3)2(Aq)+H2(g) \n\nAssinale a alternativa que indica o nome do sal formado nessa reação e que apresenta a equação da reação em que o alumínio desloca o hidrogênio do ácido clorídrico.",
+
+    respota: 1,
+    opcoes: [
+      {
+        id: 0,
+        isSelecionado: false,
+        texto: "a) Nitrato de cálcio; 2 Al+2 HCl→2 AlCl+H2"
+      }, 
+      {
+        id: 1,
+        isSelecionado: false,
+        texto: "b) Nitrato de cálcio; 2 Al+6 HCl→2 AlCl3+3 H2"
+      },
+      {
+        id: 2,
+        isSelecionado: false,
+        texto: "c) Nitrato de cálcio; 2 AlCl3+3 H2→2 Al+6 HCl2"
+      },
+      {
+        id: 3,
+        isSelecionado: false,
+        texto: "d) Dinitreto de monocálcio; 2 Al+6 HCl→AlCl+H2"
+      },
+      
+    ]
+  },
+  {
+    id: 9,
+    titulo: "Questão 9",
+    pergunta: "Qual dos metais a seguir não poderia realizar uma reação de deslocamento com o AgNO3 (nitrato de prata)?",
+
+    respota: 3,
+    opcoes: [
+      {
+        id: 0,
+        isSelecionado: false,
+        texto: "a) Al+3 HCl→"
+      }, 
+      {
+        id: 1,
+        isSelecionado: false,
+        texto: "b) Ba+2 HCl→"
+      },
+      {
+        id: 2,
+        isSelecionado: false,
+        texto: "c) 2 Li+MgBr2→"
+      },
+      {
+        id: 3,
+        isSelecionado: false,
+        texto: "d) Au+HCl→"
+      },
+      
+    ]
+  },
+  {
+    id: 10,
+    titulo: "Questão 10",
+    pergunta: "O sódio é um metal mole, de cor prateada, que reage violentamente com a água, como está equacionado a seguir: \n\n2Na(s)+2 H2O(l)→2 NaOH(aq)+H2(g) \n\nEssa reação química é identificada como:",
+
+    respota: 3,
+    opcoes: [
+      {
+        id: 0,
+        isSelecionado: false,
+        texto: "a) simples troca"
+      }, 
+      {
+        id: 1,
+        isSelecionado: false,
+        texto: "b) dupla troca"
+      },
+      {
+        id: 2,
+        isSelecionado: false,
+        texto: "c) decomposição"
+      },
+      {
+        id: 3,
+        isSelecionado: false,
+        texto: "d) adição"
+      },
+      
+    ]
+  },
 ]
 
 
